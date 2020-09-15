d78a4547d35e75802805ee602001a9351a0d0b90
Alex <helloalexsr@gmail.com>
2020-09-15 21:33:45 +0200

    ready

--- js/index.js
+++ js/index.js
@@ -1,23 +1,40 @@
-// ITERATION 1
+// ITERATION 1 
 
 function updateSubtotal(product) {
+  console.log(product)
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let price = product.querySelector('.price span').innerHTML;
+  let quantity = product.querySelector('.quantity input').value;
+  
+  let subtotal = quantity * price;
+
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+
+
+  return subtotal
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  
+   let products = Array.from(document.querySelectorAll("tr.product"))
 
+   let total = 0;
+   
+   products.forEach (product => {
+   total += updateSubtotal(product);
+  })
+
+ 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector("#total-value span").innerHTML = total;
 }
 
 // ITERATION 4
@@ -40,3 +57,24 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+
+
+// Basic
+
+document.querySelector('h1')
+
+document.querySelectorAll('.class')
+
+// document.querySelectorAll('.class').property = 'stament';
+
+// document.querySelector('button').onclick = () => {}
+
+
+// BONUS add elements *
+
+let  newElement = document.createElement(h1)
+
+newElement.innerText = "aaaa"
+
+document.querySelector('boddy').append(newElement)
